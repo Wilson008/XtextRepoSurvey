2c8fc9ba83ace0f1e63eac86ffe4d49cfab3edd8
wzhangasj <weixing.zhang@gu.se>
2024-03-27 21:35:01 +0100

    Complete the ranked data

--- rank_xtext_commit_change.py
+++ rank_xtext_commit_change.py
@@ -2,7 +2,7 @@ import csv
 
 # 读取原始数据并排序
 data = []
-with open('count_changed_lines.csv', mode='r', newline='', encoding='utf-8') as csv_file:
+with open('auto_analysis_results/count_changed_rules.csv', mode='r', newline='', encoding='utf-8') as csv_file:
     reader = csv.reader(csv_file)
     next(reader)  # 跳过header行
     for row in reader:
@@ -14,7 +14,7 @@ data_rounded = [round(value, 2) for value in data]
 sorted_data = sorted(data_rounded)
 
 # 将排序后的数据写入新的CSV文件
-with open('ranked_results.csv', mode='w', newline='', encoding='utf-8') as output_csv:
+with open('auto_analysis_results/xtext_rule_chnage_ranked.csv', mode='w', newline='', encoding='utf-8') as output_csv:
     writer = csv.writer(output_csv)
     for value in sorted_data:
         writer.writerow([value])
